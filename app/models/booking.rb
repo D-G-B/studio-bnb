3d06b9ca798b87d57b87adb1a4847e4b55786de7
Kevin Agoh <kevinagoh@hotmail.fr>
2021-02-22 17:47:30 +0100

    Models done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,15 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :studio
+  validates :end_date, :start_date, presence: true
+  validates :end_date, :start_date, uniqueness: true
+  validates :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+ end
 end
