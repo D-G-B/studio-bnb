391f60a3d7d7dc11ea054cdd52a13a7e3fa2ce85
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-02-23 13:25:50 +0100

    commented out date validations heroku bug?

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,14 +2,14 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :studio
   validates :end_date, :start_date, presence: true
-  validates :end_date, :start_date, uniqueness: true
-  validates :end_date_after_start_date
+#   validates :end_date, :start_date, uniqueness: true
+#   validates :end_date_after_start_date
 
-  private
+#   private
 
-  def end_date_after_start_date
-    if end_date < start_date
-      errors.add(:end_date, "must be after the start date")
-    end
- end
+#   def end_date_after_start_date
+#     if end_date < start_date
+#       errors.add(:end_date, "must be after the start date")
+#     end
+#  end
 end
