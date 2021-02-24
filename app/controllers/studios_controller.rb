fec4db08da6042eae8d515ece491dff43a50d5a1
Kevin Agoh <kevinagoh@hotmail.fr>
2021-02-24 13:06:43 +0100

    we can now search studios per equipment

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -7,11 +7,27 @@ class StudiosController < ApplicationController
     @search = params["search"]
     if @search.present?
       @name = @search["name"]
-      @studios = Studio.where(name: @name)
+      @studios = Studio.where("name ILIKE ?", "%#{@name}%")
     end
   end
 
   def show
     @studio = Studio.find(params[:id])
+  end
+
+  def daw
+    @studios = Studio.where(equipment: "Digital Audio workstation")
+  end
+
+  def audio_interface
+    @studios = Studio.where(equipment: "Audio interface")
+  end
+  
+  def micro
+    @studios = Studio.where(equipment: "Microphones")
+  end
+  
+  def monitor
+    @studios = Studio.where(equipment: "Monitor")
   end
 end
