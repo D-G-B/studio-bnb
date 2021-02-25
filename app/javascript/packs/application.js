bc76ee29dffce88481f25d89273413be2c0f3c28
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-02-25 18:55:06 +0100

    installed flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,9 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatpickr } from "../plugins/flatpickr";
+
+initFlatpickr();
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
