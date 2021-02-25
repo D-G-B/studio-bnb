4344e07688f1661ae6e77646140f5a78c19d09bb
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-02-25 19:44:17 +0100

    not working

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,14 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr("#range_start", {
+    allowInput: true,
+    altInput: true,
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    plugins: [new rangePlugin({ input: "#range_end"})]
+  });
 }
 
 export { initFlatpickr };
