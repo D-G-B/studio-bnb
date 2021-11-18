c7134fdf6b74374bf50a1db26839df825d35b75f
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-11-18 10:56:39 +0100

    smol syntax changes

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -5,7 +5,7 @@ class Studio < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  CATEGORIES = [ "Digital Audio workstation", "Audio interface", "Microphones", "Monitor"]
+  CATEGORIES = ["Digital Audio workstation", "Audio interface", "Microphones", "Monitor"]
   validates :name, :address, :equipment, :rate, presence: true
   validates :name, uniqueness: true
   validates :equipment, inclusion: CATEGORIES
